0d0aae0eaa97a289a22facc1e1866bdc2c114bac
maxitect <maxime.downe@gmail.com>
2024-10-18 16:03:59 +0100

    feat: challenge 3 passes tests

--- src/routes/log-in.js
+++ src/routes/log-in.js
@@ -1,5 +1,7 @@
 const { getUserByEmail } = require("../model/user.js");
+const { createSession } = require("../model/session.js");
 const { Layout } = require("../templates.js");
+const bcrypt = require("bcryptjs");
 
 function get(req, res) {
   const title = "Log in to your account";
@@ -29,14 +31,15 @@ function post(req, res) {
   if (!email || !password || !user) {
     return res.status(400).send("<h1>Login failed</h1>");
   }
-  res.send("to-do");
-  /**
-   * [1] Compare submitted password to stored hash
-   * [2] If no match redirect back to same page so user can retry
-   * [3] If match create a session with their user ID,
-   *     set a cookie with the session ID,
-   *     redirect to the user's confession page (e.g. /confessions/3)
-   */
+  bcrypt.compare(password, user.hash).then(password_check => {
+    if (password_check) {
+      const sid = createSession(user.id);
+      res.cookie('sid', sid, { signed: true, httpOnly: true, maxAge: 1000 * 60, sameSite: "lax" });
+      res.redirect(`/confessions/${user.id}`);
+    } else {
+      return res.status(400).send("<h1>Login failed</h1>");
+    }
+  });
 }
 
 module.exports = { get, post };
