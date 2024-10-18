b853a5c1eb22df806e87fb2ab110326422892f7b
maxitect <maxime.downe@gmail.com>
2024-10-18 16:49:30 +0100

    feat: challenge 5 passes test

--- src/routes/confessions.js
+++ src/routes/confessions.js
@@ -3,42 +3,42 @@ const {
   createConfession,
 } = require("../model/confessions.js");
 const { Layout } = require("../templates.js");
+const { getSession } = require("../model/session.js");
 
 function get(req, res) {
-  /**
-   * Currently any user can view any other user's private confessions!
-   * We need to ensure only the logged in user can see their page.
-   * [1] Get the session ID from the cookie
-   * [2] Get the session from the DB
-   * [3] Get the logged in user's ID from the session
-   * [4] Get the page owner from the URL params
-   * [5] If the logged in user is not the page owner send a 401 response
-   */
-  const confessions = listConfessions(req.params.user_id);
-  const title = "Your secrets";
-  const content = /*html*/ `
-    <div class="Cover">
-      <h1>${title}</h1>
-      <form method="POST" class="Stack" style="--gap: 0.5rem">
-        <textarea name="content" aria-label="your confession" rows="4" cols="30" style="resize: vertical"></textarea>
-        <button class="Button">Confess 🤫</button>
-      </form>
-      <ul class="Center Stack">
-        ${confessions
-          .map(
-            (entry) => `
-            <li>
-              <h2>${entry.created_at}</h2>
-              <p>${entry.content}</p>
-            </li>
-            `
-          )
-          .join("")}
-      </ul>
-    </div>
-  `;
-  const body = Layout({ title, content });
-  res.send(body);
+  const sid = req.signedCookies.sid;
+  const session = getSession(sid);
+  const user_id = session && session.user_id;
+  const page_owner = Number(req.params.user_id);
+  if (user_id === page_owner) {
+    const confessions = listConfessions(req.params.user_id);
+    const title = "Your secrets";
+    const content = /*html*/ `
+      <div class="Cover">
+        <h1>${title}</h1>
+        <form method="POST" class="Stack" style="--gap: 0.5rem">
+          <textarea name="content" aria-label="your confession" rows="4" cols="30" style="resize: vertical"></textarea>
+          <button class="Button">Confess 🤫</button>
+        </form>
+        <ul class="Center Stack">
+          ${confessions
+            .map(
+              (entry) => `
+              <li>
+                <h2>${entry.created_at}</h2>
+                <p>${entry.content}</p>
+              </li>
+              `
+            )
+            .join("")}
+        </ul>
+      </div>
+    `;
+    const body = Layout({ title, content });
+    res.send(body);
+  } else {
+    return res.status(401).send("<h1>You are very naughty</h1>");
+  }
 }
 
 function post(req, res) {
